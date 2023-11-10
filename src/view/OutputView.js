c51cb98b94e97f66abf1fa6160de731c7fc5b2d8
abi-hong <flffjakfwm20@gmail.com>
2023-11-11 00:59:14 +0900

    feat(Output): 공통 출력 포맷 구현

--- src/view/OutputView.js
+++ src/view/OutputView.js
@@ -1,13 +1,26 @@
 import { Console } from '@woowacourse/mission-utils';
 
 const OutputView = {
-    announcement(output) {
-        Console.print(`${output}`);
-    },
-    menu(orderList) {
-        this.announcement('<주문메뉴>');
-        orderList.map((order) => Console.print(`${order[0]} ${order[1]}개`));
-    },
+  announcement(output) {
+    Console.print(`${output}`);
+  },
+
+  moneyInfo(message, output) {
+    Console.print(`
+<${message}>
+${output}원`);
+  },
+
+  eventInfo(message, output) {
+    Console.print(`
+<${message}>
+${output}`);
+  },
+
+  menu(orderList) {
+    this.announcement('<주문메뉴>');
+    orderList.map((order) => Console.print(`${order[0]} ${order[1]}개`));
+  },
 }
 
 export default OutputView;

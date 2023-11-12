b89452d6f01d391850b1fd4715595d7deae734ac
abi-hong <flffjakfwm20@gmail.com>
2023-11-12 20:28:21 +0900

    refactor(Calculate): date 변수명 변경

--- src/model/Calculate.js
+++ src/model/Calculate.js
@@ -15,23 +15,23 @@ const Calculate = {
   },
 
   // 크리스마스 디데이 할인 계산 로직
-  christamDiscount(date) {
-    const orderDate = new Date(`2023-12-${date}`);
-    const date = orderDate.getDate();
-    if (date >= 1 && date <= 25) {
-      return 1000 + 100 * (date - 1);
+  christamDiscount(number) {
+    const orderDate = new Date(`2023-12-${number}`);
+    const dateNumber = orderDate.getDate();
+    if (dateNumber >= 1 && dateNumber <= 25) {
+      return 1000 + 100 * (dateNumber - 1);
     } 
     return 0;
   },
 
   // 평일 할인(일~목 => 0,1,2,3,4) 계산 로직
-  weekdayDiscount(date, menus) {
-    const orderDate = new Date(`2023-12-${date}`);
-    const date = orderDate.getDay();
-    if (date >= 0 && date <= 4) {
+  weekdayDiscount(number, menus) {
+    const orderDate = new Date(`2023-12-${number}`);
+    const dateNumber = orderDate.getDay();
+    if (dateNumber >= 0 && dateNumber <= 4) {
       const dessertCount = menus.reduce((prev, menu) => {
         if (menu.isDessert())
-          return prev + 1;
+          return prev + menu.isDessert();
         return prev;
       }, 0)
       return dessertCount * 2023;
@@ -39,13 +39,13 @@ const Calculate = {
     return 0;
   }, 
   // 주말 할인(금, 토 => 5,6) 계산 로직
-  weekendDiscount(date, menus) {
-    const orderDate = new Date(`2023-12-${date}`);
-    const date = orderDate.getDay();
-    if (date === 5 || date === 6) {
+  weekendDiscount(number, menus) {
+    const orderDate = new Date(`2023-12-${number}`);
+    const dateNumber = orderDate.getDay();
+    if (dateNumber === 5 || dateNumber === 6) {
       const mainCount = menus.reduce((prev, menu) => {
         if (menu.isMain())
-          return prev + 1;
+          return prev + menu.isMain();
         return prev;
       }, 0)
       return mainCount * 2023;
@@ -54,16 +54,17 @@ const Calculate = {
   },
 
   // 특별 할인 계산 → 로직이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인
-  specialDiscount(date) {
-    const orderDate = new Date(`2023-12-${date}`);
-    const date = orderDate.getDate();
-    if (SPECIALDAY.includes(date)) return 1000;
+  specialDiscount(number) {
+    const orderDate = new Date(`2023-12-${number}`);
+    const dateNumber = orderDate.getDate();
+    if (SPECIALDAY.includes(dateNumber)) return 1000;
     return 0;
   },
 
    // 총혜택 금액 계산 로직 → 총혜택 금액 = 할인 금액의 합계 + 증정 메뉴의 가격
-   totalBenefit(discountTotal, freeGift) {
-    return discountTotal + freeGift;
+   totalBenefit(discountTotal, isFreeGift) {
+    if (isFreeGift) return discountTotal + 25000;
+    return discountTotal;
    },
 
    // 이벤트 배지 계산 로직 → 총혜택 금액에 따라 다른 이벤트 배지

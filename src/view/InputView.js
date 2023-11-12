e9d64f20a442c52bf609445b47c462fca899881f
abi-hong <flffjakfwm20@gmail.com>
2023-11-12 20:27:22 +0900

    refactor(Input): 출력문 추가

--- src/view/InputView.js
+++ src/view/InputView.js
@@ -2,7 +2,7 @@ import { Console } from '@woowacourse/mission-utils';
 
 const InputView = {
     async visitDate() {
-        const input = await Console.readLineAsync('12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n');
+        const input = await Console.readLineAsync('안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)\n');
         return Number(input);
     },
     async orderMenu() {

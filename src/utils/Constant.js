452f609ad3df41003e5a00d31e6c14673169ece5
abi-hong <flffjakfwm20@gmail.com>
2023-11-14 16:15:58 +0900

    feat(Constant): 음료 메뉴 배열 상수화

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -12,13 +12,9 @@ export const MENU_LIST = [
   '레드와인',
   '샴페인'
 ]
-
-export const MENU_COURSE = {
-  appetizer : ['양송이수프', '타파스', '시저샐러드'],
-  main : ['티본스테이크', '바비큐립', '해산물파스타', '크리스마스파스타'],
-  dessert : ['초코케이크', '아이스크림'],
-  drink : ['제로콜라', '레드와인', '샴페인'],
-}
+export const MENU_DRINK = [
+  '제로콜라', '레드와인', '샴페인'
+]
 
 export const MENU_PRICE = {
   0: 6000, //'양송이수프': 6000,

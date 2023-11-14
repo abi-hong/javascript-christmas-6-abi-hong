9b8bcc7745425a69633389beac22497094723ab6
abi-hong <flffjakfwm20@gmail.com>
2023-11-14 16:30:36 +0900

    refactor(Validator, Constant): 예외 구문 상수화

--- src/utils/Constant.js
+++ src/utils/Constant.js
@@ -42,3 +42,17 @@ export const SPECIALDAY = [
   25,
   31
 ]
+
+export const BADGE = {
+  santa: '산타',
+  tree: '트리',
+  star: '별',
+  none: '없음',
+}
+
+export const EXCEPTION = {
+  day: '[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.',
+  order: '[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.',
+  count: '[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.',
+  onlyDrink: '[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.',
+}
--- src/utils/Validator.js
+++ src/utils/Validator.js
@@ -1,17 +1,17 @@
 import { Console } from "@woowacourse/mission-utils";
-import { MENU_LIST, MENU_DRINK } from './Constant.js';
+import { MENU_LIST, MENU_DRINK, EXCEPTION } from './Constant.js';
 
 const Validator = {
   checkDate(input) {
     const strInput = String(input)
     if (/^(?:[1-9]|[12]\d|3[01])$/.test(strInput)) return true;
-    Console.print('[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.');
+    Console.print(EXCEPTION.day);
     return false;
   },
   checkMenuName(nameList) {
     for (let i = 0; i < nameList.length; i++) {
       if (!MENU_LIST.includes(nameList[i])) {
-        Console.print('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+        Console.print(EXCEPTION.order);
         return false;
       }
     }
@@ -21,13 +21,13 @@ const Validator = {
     for (let i = 0; i < countList.length; i++) {
       if (/^[1-9]\d*$/.test(countList[i])) return true;
     }
-    Console.print('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    Console.print(EXCEPTION.order);
     return false;
   },
   checkNotDuplicateMenu(nameList) {
     const isNotDuplicate = nameList.filter((name, index) => nameList.indexOf(name) !== index);
     if (isNotDuplicate.length === 0) return true;
-    Console.print('[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.');
+    Console.print(EXCEPTION.order);
     return false;
   },
   checkTotalMenuCount(nameList, countList) {
@@ -37,14 +37,14 @@ const Validator = {
       return prev;
     }, 0);
     if (totalCount > 21) {
-      Console.print('[ERROR] 메뉴는 한 번에 최대 20개까지만 주문할 수 있습니다. 다시 입력해 주세요.');
+      Console.print(EXCEPTION.count);
       return false;
     }
     for (let i = 0; i < nameList.length; i++) {
       if (MENU_DRINK.includes(nameList[i])) dessertCount += 1;
     }
     if (dessertCount !== totalCount) return true;
-    Console.print('[ERROR] 음료만 주문 시, 주문할 수 없습니다. 다시 입력해 주세요.');
+    Console.print(EXCEPTION.onlyDrink);
     return false;
   },
 

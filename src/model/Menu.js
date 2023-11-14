56254a68e74d04de04fc46b84462636f58ba267d
abi-hong <flffjakfwm20@gmail.com>
2023-11-14 18:22:30 +0900

    chore(Menu): 주석 및 사용하지 않는 코드 제거

--- src/model/Menu.js
+++ src/model/Menu.js
@@ -8,15 +8,6 @@ class Menu {
     this.#name = name;
     this.#count = count;
   }
-  /** 
-   * 값을 return 하도록 구현 !!
-   */
-  getName() {
-    return this.#name;
-  }
-  getCount() {
-    return this.#count;
-  }
 
   checkMenu() {
     MENU_LIST.forEach((menu, index) => {
@@ -24,15 +15,15 @@ class Menu {
     })
   }
 
-  // 메뉴 * 수량 가격 리턴
   menuPrice() {
     return MENU_PRICE[this.#name] * this.#count;
   }
-  // 메뉴 클래스 리턴
+
   isDessert() {
     if ((this.#name === 7) || (this.#name === 8)) return this.#count;
     return 0;
   }
+  
   isMain() {
     if ((this.#name === 3) || (this.#name === 4) || (this.#name === 5) || (this.#name === 6)) return this.#count;
     return 0;
